7f3ffb428092cd2654250ccfdda5834cb9f7051c
DXVII <david.pham.d@gmail.com>
2021-03-30 23:49:04 +1100

    INVERSE KINEMATICS

--- DHTableCode.m
+++ DHTableCode.m
@@ -106,14 +106,56 @@ a3*(sin(th1)*sin(th2)*sin(th3) - cos(th2)*cos(th3)*sin(th1)) + d2*cos(th1) + d3*
 
 
 %% Inverse
-eqn1 = x == d2*sin(th1) - a3*(cos(th1)*sin(th2)*sin(th3) - cos(th1)*cos(th2)*cos(th3)) + d3*sin(th1) + d4*sin(th1) + a2*cos(th1)*cos(th2);
-eqn2 = y == a3*(sin(th1)*sin(th2)*sin(th3) - cos(th2)*cos(th3)*sin(th1)) + d2*cos(th1) + d3*cos(th1) + d4*cos(th1) - a2*cos(th2)*sin(th1);
-eqn3 = z == d1 + a3*sin(th2 + th3) + a2*sin(th2)-d5;
 
-eqm1 = subs(eqn1,{d2,d3,d4},{0,0,0})
-eqm2 = subs(eqn2,{d2,d3,d4},{0,0,0})
-eqm3 = subs(eqn3,{d2,d3,d4},{0,0,0})
+eqn1 = d2*sin(th1) - a3*(cos(th1)*sin(th2)*sin(th3) - cos(th1)*cos(th2)*cos(th3)) + d3*sin(th1) + d4*sin(th1) + a2*cos(th1)*cos(th2);
+eqn2 = a3*(sin(th1)*sin(th2)*sin(th3) - cos(th2)*cos(th3)*sin(th1)) + d2*cos(th1) + d3*cos(th1) + d4*cos(th1) - a2*cos(th2)*sin(th1);
+eqn3 = d1 + a3*sin(th2 + th3) + a2*sin(th2)-d5;
 
+%% Linearise
+% Simplifying negligible lengths
+fx = subs(eqn1,{d2,d3,d4},{0,0,0})
+fy = subs(eqn2,{d2,d3,d4},{0,0,0})
+fz = subs(eqn3,{d2,d3,d4},{0,0,0})
+
+% need to find valid equilibria
+
+% equilibrium points, (to be found)
+eq_th1 = pi/4;
+eq_th2 = pi/4;
+eq_th3 = pi/4;
+equib = {eq_th1, eq_th2, eq_th3};
+
+thetas = {th1, th2, th3};
+
+% taylor poly tings
+dfxdth1 = diff(fx, th1);
+dfxdth2 = diff(fx, th2);
+dfxdth3 = diff(fx, th3);
+
+dfydth1 = diff(fy, th1);
+dfydth2 = diff(fy, th2);
+dfydth3 = diff(fy, th3);
+
+dfzdth1 = diff(fz, th1);
+dfzdth2 = diff(fz, th2);
+dfzdth3 = diff(fz, th3);
+
+
+% taylor polynomial --> linear
+lin_x = x == subs(dfxdth1, thetas, equib) * th1 + subs(dfxdth2, thetas, equib) * th2 + subs(dfxdth3, thetas, equib) * th3
+lin_y = y == subs(dfydth1, thetas, equib) * th1 + subs(dfydth2, thetas, equib) * th2 + subs(dfydth3, thetas, equib) * th3
+lin_z = z == subs(dfzdth1, thetas, equib) * th1 + subs(dfzdth2, thetas, equib) * th2 + subs(dfzdth3, thetas, equib) * th3
+
+[A, B] = equationsToMatrix([lin_x, lin_y, lin_z], [th1, th2, th3]);
+
+decoupled = inv(A)*B;
+th1 = decoupled(1)
+th2 = decoupled(2)
+th3 = decoupled(3)
 
 % solve(eq1, eq2, eq3,[th1,th2,th3])
 % [A,B] = equationsToMatrix([eqn1, eqn2, eqn3], [th1, th2, th3]);
+
+% x == a2 * cos(th1) * cos(th2) - a3 * (cos(th1) * sin(th2) * sin(th3) - cos(th1) * cos(th2) * cos(th3))
+% y == a3 * (sin(th1) * sin(th2) * sin(th3) - cos(th2) * cos(th3) * sin(th1)) - a2 * cos(th2) * sin(th1)
+% z == d1 - d5 + a3 * sin(th2 + th3) + a2 * sin(th2)
