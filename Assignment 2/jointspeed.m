6a588fc076edfaad6f24600e540319d5acf9707e
Bongcloud420 <80876634+Bongcloud420@users.noreply.github.com>
2021-04-15 18:29:28 +1000

    Add files via upload
    Finalised code but spits out gross answers that will probably be corrected by hand

--- Assignment 2/jointspeed.m
+++ Assignment 2/jointspeed.m
@@ -32,3 +32,58 @@ z6 = R06*z;
 
 Z_mat = [z1 z2 z3 z4 z5 z6];
 % Z_mat_fin = vpa(Z_mat,3)
+%% Linear
+t = @(x,y,z) [x;y;z];
+RxA = @(angle,x,y,z) [Rx(angle), t(x,y,z); 0, 0, 0, 1];
+RzA = @(angle,x,y,z) [Rz(angle), t(x,y,z); 0, 0, 0, 1];
+
+syms a alpha d theta
+transNext = @(a, alpha, d, theta) RxA(0,a,0,0)*RxA(alpha,0,0,0)*RzA(0,0,0,d)*RzA(theta,0,0,0);
+
+syms d1 th1 d2 th2 a2 d3 th3 a3 d4 th4 d5 th5 d6
+DHtable =   [0 0 d1 th1; ...            % frame 0-1: XY swivel
+            0 pi/2 d2 th2; ...          % frame 1-2: XZ bicep elevation
+            a2 0 d3 th3; ...            % frame 2-3: XZ elbow descent
+            a3 0 d4 th4; ...            % frame 3-4: XZ vertical wrist (constraint: point to ground)
+            0 pi/2 d5 th5; ...          % frame 4-5: XY wrist torsional twist (constraint: point foward)
+            0 0 d6 0];                  % frame 5-6: Wrist Gripper to distance 
+
+final = eye(4);             % being DH chain
+
+f_final = length(DHtable);  % code tings: simplify end of array 
+
+% frame 1 to 4 chosen due to relational constrains of frames 4 and onwards (discussed in report)
+frame_start = 1;            % code tings: start of transform chain
+frame_stop = 6;             % code tings: end of transform chain
+
+translations = sym([0 0 0;0 0 0;0 0 0;0 0 0;0 0 0;0 0 0]);
+
+for row = frame_start:1:frame_stop    
+    a = DHtable(row,1);
+    alpha = DHtable(row,2);
+    d = DHtable(row,3);
+    theta = DHtable(row,4);
+    
+    fprintf("Matrix: %d to %d",row-1,row)
+    tmpMat(1:4,1:4) = transNext(a, alpha, d, theta)
+   translations(row,:) = transpose(tmpMat(1:3,4));
+    final = final*transNext(a, alpha, d, theta);
+end
+%Creating vectors between frames
+r01_0 = translations(1,:);
+r12_1 = translations(2,:);
+r23_2 = translations(3,:);
+r34_3 = translations(4,:);
+r45_4 = translations(5,:);
+r56_5 = translations(6,:);
+
+%Creating vectors from frame to end effector in 0 frame orientation
+
+r56_0 = R05*r56_5';
+r46_0 = R04*r45_4' + r56_0;
+r36_0 = R03*r34_3' + r46_0;
+r26_0 = R02*r23_2' + r36_0;
+r16_0 = R01*r12_1' + r26_0;
+r06_0 = r01_0' + r26_0; %unnecessary
+
+Jv = [cross(r16_0,z1) cross(r26_0,z2) cross(r36_0,z3) cross(r46_0,z4) cross(r56_0,z5)];
